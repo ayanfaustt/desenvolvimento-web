b736e2be3d707d9eebcb7aaf63a0895862622d0b
Diogo de Souza <di_smgomes@hotmail.com>
2023-07-07 17:40:21 -0300

    created dashboard page

---
+++ frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './styles.css';
+import SideBar from '../../components/sidebar';
+
+interface DashboardPageProps {
+
+}
+export default function DashboardPage(props: DashboardPageProps) {
+    return (
+        <main className='sidebar-content'>
+            <SideBar></SideBar>
+            <div className='dashboard-content'>
+                <div className='box-center'>
+                    <div className='content-inside'>
+
+                        <div className='text-inside'>
+                            <p className='welcome'>Welcome, userx! </p>
+                            <p className='view'>View an overview of your progress.</p>
+                        </div>
+
+                        <div className='overview'>
+
+                            <div className='left-content'>
+                                <div className='performace'></div>
+
+                                <div className='left-content-inside'>
+                                    <div className='events'></div>
+                                    <div className='usedResources'></div>
+                                </div>
+                            </div>
+
+                            <div className='right-content'></div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
