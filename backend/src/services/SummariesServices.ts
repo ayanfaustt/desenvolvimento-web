0299cfa152564f504687bc239183586236b9a99b
ayanfaustt <ayanfaustt@gmail.com>
2023-08-03 14:11:14 -0300

    test

--- backend/src/services/SummariesServices.ts
+++ backend/src/services/SummariesServices.ts
@@ -1,5 +1,5 @@
 import { Model } from "sequelize";
-import SummarieRepository from "../database/repositories/summarieRepository";
+import SummarieRepository from "../database/repositories/SummarieRepository";
 import { gpt } from "./external/openai";
 
 
