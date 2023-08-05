8e9c02db7a236a7925d4b0ec7457842efdba225f
ayanfaustt <ayanfaustt@gmail.com>
2023-08-05 12:54:44 -0300

    adding lint

--- backend/src/database/repositories/cardRepository.ts
+++ backend/src/database/repositories/cardRepository.ts
@@ -1,69 +1,69 @@
-import { Model, where } from "sequelize";
+import { Model } from "sequelize";
 import { CardsModel } from "../../models/cards.model";
 
 
 
 export const createCard = async (deckId: string, cardName: string, cardContent: string): Promise<void> => {
 
-    const isCardCreated = await CardsModel.create({
-        deckId: deckId,
-        card_name: cardName,
-        card_content: cardContent
-    });
+  const isCardCreated = await CardsModel.create({
+    deckId: deckId,
+    card_name: cardName,
+    card_content: cardContent
+  });
 
-    if(!isCardCreated) throw new Error("Card can not be created !")
+  if(!isCardCreated) throw new Error("Card can not be created !");
 };
 
 export const getCard = async (cardId: string): Promise<Model> => {
-    const card = await CardsModel.findOne({
-        where: {
-            id : cardId
-        }
-    });
+  const card = await CardsModel.findOne({
+    where: {
+      id : cardId
+    }
+  });
 
-    if(!card) throw new Error("Card not found !");
+  if(!card) throw new Error("Card not found !");
 
-    return card;
+  return card;
 };
 
 export const listCards = async (deckId: string): Promise<Model[]> => {
-    try{
-        const cards = await CardsModel.findAll({
-            where:{
-                deckId: deckId
-            }
-        });
+  try{
+    const cards = await CardsModel.findAll({
+      where:{
+        deckId: deckId
+      }
+    });
 
-        return cards;
-    }catch (error) {
-        throw new Error("The operation can not be completed !")
-    }
+    return cards;
+  }catch (error) {
+    throw new Error("The operation can not be completed !");
+  }
 };
 
 export const updateCard = async (deckId: string, cardName: string, cardContent: string): Promise<void> =>{
-    try {
-        await CardsModel.update(
-        {
-            card_name: cardName,
-            card_content: cardContent    
-        },{
-            where: {
-                deckId: deckId
-            }
-        });
-    } catch (error) {
-        throw new Error("The operation can not be completed !")
-    }
+  try {
+    await CardsModel.update(
+      {
+        card_name: cardName,
+        card_content: cardContent    
+      },{
+        where: {
+          deckId: deckId
+        }
+      });
+  } catch (error) {
+    throw new Error("The operation can not be completed !");
+  }
 };
 
 export const deleteCard = async (cardId: string): Promise<void> =>{
-    try {
-        await CardsModel.destroy({
-            where: {
-                id: cardId
-            },
-        });
-    } catch (error) {
-        throw new Error("The operation can not be completed !")
-    }
+  try {
+    await CardsModel.destroy({
+      where: {
+        id: cardId
+      },
+    });
+  } catch (error) {
+    throw new Error("The operation can not be completed !");
+  }
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,25 @@
 import express from "express";
 import { db } from "./database/db";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 import { router } from "./router";
 import { DateObserver}  from "./system/DateObserver";
 
-const server = express()
+const server = express();
 const { SERVER_PORT } = process.env;
   
 server.use(express.json());
 server.use(cors({
-    origin: "*",
-    optionsSuccessStatus: 200
+  origin: "*",
+  optionsSuccessStatus: 200
 }));
 server.use(router);
 
 server.listen(SERVER_PORT || "3001", async () => {
-    db.sync({
-        alter: true
-    });
-    const dateObserver = new DateObserver();
-    dateObserver.startObserving();
-    console.log(`server running in ${SERVER_PORT} port...`);
+  db.sync({
+    alter: true
+  });
+  const dateObserver = new DateObserver();
+  dateObserver.startObserving();
+  console.log(`server running in ${SERVER_PORT} port...`);
 });
